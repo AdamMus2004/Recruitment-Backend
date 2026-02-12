b24de40b27f065c3f32003c2e5926aaebeae2960
Adam Mus <adammuspraca@gmail.com>
2026-02-12 17:24:16 +0100

    feat: Add h2 database properties and dependencies

--- src/main/java/com/github/adammus2004/solarwindsrecruitmenttask/model/Response.java
+++ src/main/java/com/github/adammus2004/solarwindsrecruitmenttask/model/Response.java
@@ -3,9 +3,7 @@ package com.github.adammus2004.solarwindsrecruitmenttask.model;
 import java.util.List;
 
 public record Response(
-
             String name,
             String type,
             String url
-
 ){}
