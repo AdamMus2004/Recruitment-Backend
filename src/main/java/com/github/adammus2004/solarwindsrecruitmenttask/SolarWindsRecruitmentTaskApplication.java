db3420e4893185f57123418b5700ff7ce4692ebb
Adam Mus <adammuspraca@gmail.com>
2026-02-12 16:24:46 +0100

    initial commit

---
+++ src/main/java/com/github/adammus2004/solarwindsrecruitmenttask/SolarWindsRecruitmentTaskApplication.java
@@ -0,0 +1,13 @@
+package com.github.adammus2004.solarwindsrecruitmenttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolarWindsRecruitmentTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolarWindsRecruitmentTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/adammus2004/solarwindsrecruitmenttask/SolarWindsRecruitmentTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.adammus2004.solarwindsrecruitmenttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolarWindsRecruitmentTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
