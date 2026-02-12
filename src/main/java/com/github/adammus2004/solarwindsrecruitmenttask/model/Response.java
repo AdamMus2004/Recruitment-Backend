dea53d824bfa847bb575bfcf8e4676f3d3d90a8a
Adam Mus <adammuspraca@gmail.com>
2026-02-12 16:57:52 +0100

    fix: Response record

--- src/main/java/com/github/adammus2004/solarwindsrecruitmenttask/model/Response.java
+++ src/main/java/com/github/adammus2004/solarwindsrecruitmenttask/model/Response.java
@@ -3,12 +3,9 @@ package com.github.adammus2004.solarwindsrecruitmenttask.model;
 import java.util.List;
 
 public record Response(
-        List<ResponseData> responses
-) {
-    public record ResponseData(
+
             String name,
             String type,
             String url
-    )
-    {}
-}
+
+){}
